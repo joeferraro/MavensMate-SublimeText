4a1fb5aa6671421ca03e94f97858464fae9a3d90
Joe Ferraro <jf31782@gmail.com>
2012-06-28 20:17:21 -0400

    set mm_workspace for context

--- commands/clean_project.rb
+++ commands/clean_project.rb
@@ -2,6 +2,6 @@
 require File.dirname(File.dirname(__FILE__)) + "/constants.rb"
 include Constants
 ENV["MM_CURRENT_PROJECT_DIRECTORY"] = ARGV[0]
-#ENV["TM_PROJECT_DIRECTORY"] = "/Users/josephferraro/Development/st/roooo"
+ENV["MM_WORKSPACE"] = ARGV[1]
 require LIB_ROOT + "/mavensmate.rb"
 MavensMate.clean_project({:update_sobjects => true})
