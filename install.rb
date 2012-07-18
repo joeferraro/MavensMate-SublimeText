c8ba86b2acc788100decb8eeac562816898bbfac
Joe Ferraro <jf31782@gmail.com>
2012-07-18 12:45:31 -0400

    better handle RVM

--- install.rb
+++ install.rb
@@ -20,13 +20,23 @@ module OS
 end
 
 @install_path = File.expand_path("~/Library/Application Support/Sublime Text 2/Packages/MavensMate")
+@user_settings_path = File.expand_path("~/Library/Application Support/Sublime Text 2/Packages/User")
+
+def install_package
+  `git clone git://github.com/joeferraro/MavensMate-SublimeText.git '#{@install_path}'`
+end
+
+def install_user_settings
+  `cp '#{@install_path}/mavensmate.sublime-settings' '#{@user_settings_path}'` unless File.exist?("#{@user_settings_path}/mavensmate.sublime-settings")
+end
 
 def install
 	if OS.windows?
 		#future functionality
 	elsif OS.mac?
-		`git clone git://github.com/joeferraro/MavensMate-SublimeText.git '#{@install_path}'`
-	elsif OS.linux?
+		install_package
+    install_user_settings
+  elsif OS.linux?
 		#future functionality
 	end
 end
