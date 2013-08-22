c704eb6a0633f0bf8115dc7d48128a9422bdfac5
Joe Ferraro <jf31782@gmail.com>
2013-08-22 13:52:04 +0200

    install scripts

--- install-dev.py
+++ install-dev.py
@@ -22,10 +22,13 @@ try:
     platform            = sys.platform
     install_path        = install_paths[platform]
     user_settings_path  = user_settings_path[platform]
+    branch              = 'dev'
     git_url             = pipes.quote('git://github.com/joeferraro/MavensMate-SublimeText.git')
 
     def install_from_source():
         os.system("git clone {0} {1}".format(git_url, pipes.quote(install_path)))
+        os.chdir(install_path)
+        os.system("git checkout -b {0} origin/{0}".format(pipes.quote(branch)))
 
     def install_user_settings():
         if os.path.isfile(user_settings_path+"/mavensmate.sublime-settings") == False:
@@ -42,7 +45,13 @@ try:
         install_from_source()
         install_user_settings()
 
-    if __name__ == '__main__':        
+    if __name__ == '__main__':
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-b', '--branch') #name of the branch being requested
+        args = parser.parse_args()
+        print args
+        if args.branch != None and args.branch != '':
+            branch = args.branch
         install()
 except Exception as e:
     print('install.py issue')
