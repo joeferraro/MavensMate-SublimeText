452cf0dacfbd86888abc5d20f70d3a245088a4ab
Joe Ferraro <jf31782@gmail.com>
2013-08-22 08:59:47 +0200

    make mm_dir obvious

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 import os
 import sublime
 
-try:
-    mm_dir = os.getcwdu()
-except:
-    mm_dir = os.path.dirname(__file__)
-
+mm_dir = os.path.dirname(__file__)
 sublime_version = int(float(sublime.version()))
 settings = None
 merge_settings = None
