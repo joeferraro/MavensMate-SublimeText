2d5d12e9d18f2cb584d263b9aaee507eefed7da1
Joe Ferraro <jf31782@gmail.com>
2013-09-19 18:46:36 +0200

    handle mavensmate being installed into x86 program file refs #217

--- lib/mm_interface.py
+++ lib/mm_interface.py
@@ -273,6 +273,8 @@ class MavensMateTerminalCall(threading.Thread):
             else:
                 #windows
                 python_path = os.path.join(os.environ["ProgramFiles"],"MavensMate","App","python")
+                if not os.path.exists(python_path):
+                    python_path = python_path.replace("Program Files", "Program Files (x86)")
                 process = subprocess.Popen('"{0}" "{1}" {2}'.format(python_path, mm_loc, self.get_arguments()), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
             #process = subprocess.Popen("{0} {1} {2}".format(python_path, pipes.quote(mm_loc), self.get_arguments()), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
         else:
