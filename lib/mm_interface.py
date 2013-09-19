3411fba6cd381a4263ad8032088fe61722081703
Joe Ferraro <jf31782@gmail.com>
2013-09-19 19:34:47 +0200

    forgot extension

--- lib/mm_interface.py
+++ lib/mm_interface.py
@@ -273,7 +273,7 @@ class MavensMateTerminalCall(threading.Thread):
                 process = subprocess.Popen('\'{0}\' \'{1}\' {2}'.format(python_path, mm_loc, self.get_arguments()), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
             else:
                 #windows
-                python_path = os.path.join(os.environ["ProgramFiles"],"MavensMate","App","python")
+                python_path = os.path.join(os.environ["ProgramFiles"],"MavensMate","App","python.exe")
                 if not os.path.isfile(python_path):
                     python_path = python_path.replace("Program Files", "Program Files (x86)")
                 print('[MAVENSMATE] executing mm terminal call:')
