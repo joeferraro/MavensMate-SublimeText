a1bba2dd2e09a20af2c73dfb94630c66177e0e69
Joe Ferraro <jf31782@gmail.com>
2013-12-19 19:47:09 +0100

    better plugin logging with rotating file handler

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 import sublime
 import logging
+from logging.handlers import RotatingFileHandler
 import tempfile
 
 mm_dir = os.path.dirname(__file__)
@@ -8,13 +9,29 @@ sublime_version = int(float(sublime.version()))
 settings = None
 merge_settings = None
 
-logging.raiseExceptions = False
-logging.basicConfig(level=logging.INFO)
+logger = None
 
-logging_handler = logging.FileHandler(os.path.join(tempfile.gettempdir(),"mmst.log"))
+def setup_logging():
+    settings = sublime.load_settings('mavensmate.sublime-settings')
 
-#mm log setup
-logger = logging.getLogger('mmst')
-logger.setLevel(logging.DEBUG)
-logger.propagate = False 
-logger.addHandler(logging_handler)
+    logging.raiseExceptions = False
+    logging.basicConfig(level=logging.DEBUG)
+
+    log_location = settings.get('mm_log_location', tempfile.gettempdir())
+    logging_handler = RotatingFileHandler(os.path.join(log_location, "mmst.log"), maxBytes=1*1024*1024, backupCount=5)
+
+    #mm log setup
+    global logger
+    logger = logging.getLogger('mmst')
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False 
+    logger.addHandler(logging_handler)
+
+def debug(msg, obj=None):
+    if obj != None and type(msg) is str:
+        logger.debug(msg + ' ', obj)
+        print('[MAVENSMATE]: ' + msg + ' ', obj)
+    elif obj == None and type(msg) is str:
+        logger.debug(msg)
+        print('[MAVENSMATE]: ',msg)
+       
