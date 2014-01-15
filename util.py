8d1082a87256a2f7e3565ce95e47676f4a26d88a
Joe Ferraro <jf31782@gmail.com>
2014-01-15 11:36:02 +0100

    better mm_file detection
    resolves joeferraro/MavensMate#170

--- util.py
+++ util.py
@@ -263,9 +263,10 @@ def is_mm_file(filename=None):
         if is_mm_project():
             if not filename: 
                 filename = get_active_file()
-            if "src/documents" in filename or "src\\documents" in filename:
+            project_directory = mm_project_directory(sublime.active_window())
+            if os.path.join(project_directory,"src","documents") in filename:
                 return True
-            if os.path.exists(filename):
+            if os.path.exists(filename) and os.path.join(project_directory,"src") in filename:
                 settings = sublime.load_settings('mavensmate.sublime-settings')
                 valid_file_extensions = settings.get("mm_apex_file_extensions", [])
                 if get_file_extension(filename) in valid_file_extensions and 'apex-scripts' not in get_active_file():
@@ -273,7 +274,7 @@ def is_mm_file(filename=None):
                 elif "-meta.xml" in filename:
                     return True
     except Exception as e:
-        traceback.print_exc()
+        #traceback.print_exc()
         pass
     return False
 
