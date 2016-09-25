de77169b3c77ab3845121ce7e92bf00777711112
Joe Ferraro <ferraro.joseph@gmail.com>
2016-09-25 19:14:40 -0400

    remove install script

--- bin/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -s https://mavensmate-app-auto-updater.herokuapp.com/download/channel/$2/osx > "$1/MavensMate.zip"
-unzip -a "$1/MavensMate.zip" -d "$1"
-rm -rf /Applications/MavensMate.app
-cp -R "$1/MavensMate.app" /Applications
-rm -rf "$1/MavensMate.zip"
-rm -rf "$1/MavensMate.app"
-open /Applications/MavensMate.app
